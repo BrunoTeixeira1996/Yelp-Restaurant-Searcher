6a0366d72cb03ab9532ac2443c029313084c1042
Bruno Teixeira <brunoalexandre3@hotmail.com>
2019-08-16 20:54:59 +0100

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 2:
     sys.exit()
 
 city = ' '.join(sys.argv[1:])
-key = "8X1nmozCgd2_7ERFZilmKeJXWOjDaLrvQysXcYgsIH9AgFRcIuln9ggp912I7lQ5S7c3TdVA33EK9G2bEq4SC1dh4uG0CoWCDM7kzXrfreY1vvJKTB66lrlSSqRXW3Yx"
+key = "YOURKEY"
 url = "https://api.yelp.com/v3/businesses/search?location=%s" % (city)
 header = {"Authorization":"Bearer %s" %key}
 data = requests.get(url, headers=header).json()
